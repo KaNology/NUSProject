113535c14844d3ece7a016bb4d6d4b3e6e020c48
KaNology <an.le091000@gmail.com>
2021-07-21 21:49:01 +0700

    Feed Commit

--- FrontEnd/screens/Feeds/Feeds.js
+++ FrontEnd/screens/Feeds/Feeds.js
@@ -1,4 +1,26 @@
-$('.post-content-container').on("click", function(){
+carousel = `<div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-interval="10000">
+        <img src="..." class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-interval="2000">
+        <img src="..." class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>`
+
+$('.photo').on("click", function(){
 	title = $(this).find('.post-header').text();
 	image = $(this).find('.post-image').html();
 	content = $(this).find('.post-body').text();
@@ -11,5 +33,37 @@ $('.post-content-container').on("click", function(){
 	$('.modal-footer').html(
 		content
 		);
-	console.log('selected')
+});
+
+$('.album').on("click", function(){
+	title = $(this).find('.post-header').text();
+	image = $(this).find('.post-image').html();
+	content = $(this).find('.post-body').text();
+	$('.modal-title').html(
+		title
+		);
+	$('.modal-body').html(
+		image
+		);
+	$('.modal-footer').html(
+		content
+		);
+});
+
+$('.selection-button').click(function(){
+    if($(this).hasClass('active')){
+        $(this).removeClass('active')
+    } else {
+    	$('button').removeClass('active')
+        $(this).addClass('active')
+
+        if($(this).hasClass('photo-button')){
+        	$('.album-container').hide(1000);
+        	$('.photo-container').show(1000);
+        }
+        else if ($(this).hasClass('album-button')) {
+        	$('.album-container').show(1000);
+        	$('.photo-container').hide(1000);
+        }
+    }
 });
